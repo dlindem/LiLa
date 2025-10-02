68c232bc5591ae5b23b8cddc306459220c7d10fb
David Lindemann <34068491+dlindem@users.noreply.github.com>
2025-10-02 17:07:46 +0200

    Update match_token_form.py

--- match_token_form.py
+++ match_token_form.py
@@ -3,7 +3,7 @@ import csv
 import pandas as pd
 import time
 
-data = pd.read_csv("ITTB_lemmata_wikibase.csv", sep="\t")
+data = pd.read_csv("lemmata_wikibase.csv", sep="\t")
 length = data.shape[0]
 rowcount = 1
 thislemma = None
@@ -11,7 +11,7 @@ lemmadict = {}
 while rowcount < length:
     rowdata = data.iloc[rowcount].to_dict()
     # print(rowdata)
-    lila_lemma = str(rowdata['lila_lemma_id'])
+    lila_lemma = str(rowdata['lila_lemma_id']).replace("lemma/","")
     if lila_lemma != thislemma: # new lemma list begins
         thislemma = lila_lemma
         lemmadict[thislemma] = [rowdata]
@@ -21,6 +21,7 @@ while rowcount < length:
 
 # print(list(lemmadict.keys()))
 
+matchcount = 0
 with open("ITTB_verb_tokendata.csv") as file:
     tokenrows = csv.DictReader(file, delimiter="\t")
     for row in tokenrows:
@@ -28,10 +29,30 @@ with open("ITTB_verb_tokendata.csv") as file:
         if str(row['lila_lemma']) not in lemmadict:
             continue
         lemmadict_entry = lemmadict[row['lila_lemma']]
-        token_features = row['features'].split("|")
+        token_features = sorted(row['features'].split("|"))
+        # for ignore_feature in ["VerbForm#Fin", "Degree#Pos", "Aspect#Imp"]:
+        #     if ignore_feature in token_features:
+        #         token_features.remove(ignore_feature)
+
         for formdata in lemmadict_entry:
-            if sorted(token_features) == sorted(formdata['udp_list'].split("|")):
-                print(f"Found match: token {row['tokenLabel']} = {formdata['formrep']}")
+            form_features = sorted(formdata['udp_list'].split("|"))
+
+            if formdata['formrep'] == row['tokenLabel'].replace("v", "u"):
+
+                for ignore_feature in ["Aspect#Imp"]:
+                    if ignore_feature in form_features:
+                        form_features.remove(ignore_feature)
+
+                match = True
+                for feature in form_features:
+                    if feature not in token_features:
+                        match = False
+
+                if match:
+                    matchcount += 1
+                    print(f"\nMatching token: {row['tokenLabel']} {token_features}\nMatching form:  {formdata['formrep']} {form_features}")
+
+print(f"\nFound {matchcount} matches.")
 
 
 
@@ -53,17 +74,15 @@ with open("ITTB_verb_tokendata.csv") as file:
 
 
 
+query = """PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+        PREFIX lmwb: <https://lilamorph.wikibase.cloud/entity/>
+        PREFIX lmdp: <https://lilamorph.wikibase.cloud/prop/direct/>
 
-
-# query = """PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-#         PREFIX lmwb: <https://lilamorph.wikibase.cloud/entity/>
-#         PREFIX lmdp: <https://lilamorph.wikibase.cloud/prop/direct/>
-#
-#         select distinct ?flexeme ?form ?formrep
-#         (group_concat(distinct ?udp;SEPARATOR="|") as ?udp_list)
-#         where {
-#           ?flexeme lmdp:P1 'lemma/""" + str(lemma_id) + """'.
-#           ?flexeme ontolex:lexicalForm ?form.
-#           ?form ontolex:representation ?formrep; wikibase:grammaticalFeature ?form_feat.
-#           ?form_feat lmdp:P9 [lmdp:P7 ?udp] .
-#         } group by ?flexeme ?lemma_id ?form ?formrep ?udp_list"""
+        select distinct ?lila_lemma_id ?flexeme ?form ?formrep
+        (group_concat(distinct ?udp;SEPARATOR="|") as ?udp_list)
+        where {
+          ?flexeme lmdp:P1 ?lila_lemma_id.
+          ?flexeme ontolex:lexicalForm ?form.
+          ?form ontolex:representation ?formrep; wikibase:grammaticalFeature ?form_feat.
+          ?form_feat lmdp:P9 [lmdp:P7 ?udp] .
+        } group by ?lila_lemma_id ?flexeme ?form ?formrep ?udp_list"""
