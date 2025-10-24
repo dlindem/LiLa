f47bd3aa3a09517d0ba43d455b57fe2d177bd9b8
David Lindemann <34068491+dlindem@users.noreply.github.com>
2025-10-24 15:57:47 +0200

    update

--- write_matches.py
+++ write_matches.py
@@ -2,6 +2,7 @@ import csv, time, sys
 import config_private
 from wikibaseintegrator import WikibaseIntegrator, wbi_login, datatypes
 from wikibaseintegrator.wbi_config import config
+from wikibaseintegrator.wbi_enums import ActionIfExists
 config['MEDIAWIKI_API_URL'] = "https://lilamorph.wikibase.cloud/w/api.php"
 config['USER_AGENT'] = "upload_prinparlat_lexemes.py"
 print("Getting logged into lilamorph wikibase...")
@@ -17,7 +18,9 @@ with open('data/token-form-links-2f.csv') as file:
         else:
             done_links[row['token']].append(row['linked_form'])
 
-with open('data/matching_tokens_lexemedict_v2c.csv') as file:
+
+# mapping = ""
+with open('data/matching_tokens_lexemedict_v2f.csv') as file:
     rows = csv.DictReader(file, delimiter="\t")
     count = 0
     for row in rows:
@@ -28,10 +31,14 @@ with open('data/matching_tokens_lexemedict_v2c.csv') as file:
             if matching_form in done_links[row['token_qid']]:
                 # print(f"Link is already there, no need to write.")
                 continue
+        # mapping += f"{row['token_qid']}\tP21\t{matching_form}\n"
         entity = lilamorph.item.get(entity_id=row['token_qid'])
-        entity.claims.add(datatypes.Form(prop_nr="P21", value=matching_form))
+        entity.claims.add(datatypes.Form(prop_nr="P21", value=matching_form), action_if_exists=ActionIfExists.FORCE_APPEND)
         entity.write()
         print(f"\nNow processing row [{count}]: {row}")
         print(f"Successfully written claim to {entity.id}")
         time.sleep(1)
+
+# with open('data/p21e.csv', 'w') as file:
+#     file.write(mapping)
 
