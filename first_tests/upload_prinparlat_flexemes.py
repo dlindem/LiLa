ee741a812e3a8e027e13b71e4b6da4c4b0ff0499
David Lindemann <34068491+dlindem@users.noreply.github.com>
2025-10-29 11:13:20 +0100

    new folder structure

---
+++ first_tests/upload_prinparlat_flexemes.py
@@ -0,0 +1,131 @@
+import csv, sys, time, re, json
+
+# wikibaseintegrator for lilamorph wikibase
+from wikibaseintegrator import WikibaseIntegrator, wbi_login, datatypes
+from wikibaseintegrator.models import Reference, References, Form, Sense
+from wikibaseintegrator.wbi_config import config
+import config_private # bot user and pwd from hidden file
+config['MEDIAWIKI_API_URL'] = "https://lilamorph.wikibase.cloud/w/api.php"
+config['USER_AGENT'] = "upload_prinparlat_lemmata.py"
+print("Getting logged into lilamorph wikibase...")
+lilamorph = WikibaseIntegrator(login=wbi_login.Login(user=config_private.wbuser, password=config_private.wbpwd))
+
+# get prinparlat cell descriptors mapping
+with open("mappings/paralex_featurevalue_mapping.csv") as file:
+    mappingrows = csv.reader(file, delimiter="\t")
+    leipzig_qid = {}
+    for row in mappingrows:
+        leipzig_qid[row[0]] = row[1]
+print(f"Leipzig cell descriptors mapping: {leipzig_qid}")
+
+# get prinparlat cell types default order
+with open("mappings/celltypes_sorted.txt") as file:
+    celltypes_sorted = file.read().split("\n")
+cellfeatures = {}
+for celltype in celltypes_sorted:
+    cellfeatures[celltype] = []
+    for val in celltype.split("."):
+        cellfeatures[celltype].append(leipzig_qid[val])
+print(f"cellfeatures dict: {cellfeatures}")
+
+# get prinparlat cell types default order
+with open("data/prinparlat_lexemes_canonical_forms.csv") as file:
+
+
+
+# # get unique lila lemma ID from prinparlat big file
+# print("Reading prinparlat forms...")
+# with open("enhanced_forms.csv") as file:
+#     csvrows = csv.DictReader(file, delimiter=",")
+#     flexeme_ids = []
+#     for row in csvrows:
+#         if "#DEF#" in str(row): # Defectives in irregulars (e.g. deponents) - lines are skipped
+#             continue
+#         try:
+#             flexeme_ids.append(row['flexeme']) # In some rows, instead of a single lila_id convertible to int, there appears a python list (always ['114215', '114214']) - lines are skipped
+#         except Exception as ex:
+#             print(str(ex))
+#     print(f"Got prinparlat flexeme ID from {len(flexeme_ids)} csv rows.")
+#     flexeme_unique_ids = set(flexeme_ids)
+#     print(f"That is {len(flexeme_unique_ids)} unique IDs.")
+#     flexeme_unique_ids_sorted = sorted(flexeme_unique_ids)
+
+# read mappingfile (lexemes created in former runs of the script)
+with open("mappings/prinparlat_wikibase_mapping_flexemes.csv", "r") as mappingfile:
+    mappingrows = csv.DictReader(mappingfile, delimiter="\t")
+    done_flexeme_ids = []
+    for row in mappingrows:
+        done_flexeme_ids.append(row['flexeme'])
+    print(f"{len(done_flexeme_ids)} flexeme IDs have been processed in former runs:\n{done_flexeme_ids}")
+
+# build forms dictionary
+with open("data/enhanced_forms.csv") as file:
+    not_found = 0
+    csvrows = csv.DictReader(file, delimiter=",")
+    flexemes = {}
+    for row in csvrows:
+        flexeme_id = row['flexeme']
+        if flexeme_id not in flexemes:
+            flexemes[flexeme_id] = {}
+        flexemes[flexeme_id][row['cell']] = row
+    print(f"Flexemes lexicon built ({len(flexemes)} flexemes).")
+
+for flexeme in flexemes:
+    print(f"\nNow processing flexeme: {flexeme}")
+    if flexeme in done_flexeme_ids:
+        print(f"{flexeme} flexeme has been done before (found in mappingfile), skipped.")
+        continue
+    if "prs.act.ind.1.sg" not in flexemes[flexeme] or "prs.pass.ind.1.sg" not in flexemes[flexeme]:
+        print(f"flexeme {flexeme} has no 1.p. in present, skipped in this run.")
+        with open("data/enhanced_forms_skipped_flexemes_flexemes_upload.txt", "a") as logfile:
+            logfile.write(f"{flexeme}\n")
+        continue
+    rowcount = 0
+    wikibase_lemma = None
+    new_lexeme = None
+    # ask for forms data from the formypes (celltypes) canonical order list
+    for celltype in celltypes_sorted:
+        if celltype in flexemes[flexeme]:
+            rowcount += 1
+            rowdata = flexemes[flexeme][celltype]
+
+            if not new_lexeme: #create flexeme entry
+                new_lexeme = lilamorph.lexeme.new(language="Q3", lexical_category="Q5")  # latin verb
+                new_lexeme.claims.add(datatypes.ExternalID(prop_nr="P6", value=flexeme))
+                new_lexeme.claims.add(datatypes.ExternalID(prop_nr="P8", value=rowdata['lexeme']))
+                new_lexeme.claims.add(datatypes.ExternalID(prop_nr="P1", value=f"lemma/{rowdata['lila_id_lemma']}"))
+
+            if rowdata['form_normalized'] == "#DEF#": # do not write rows with that to Wikibase
+                continue
+            if not wikibase_lemma and celltype == "prs.act.ind.1.sg":
+                wikibase_lemma = rowdata['form_normalized']
+                print(f"Row {rowcount}: Found for lemma cell {celltype}: {rowdata['form_normalized']}")
+            if not wikibase_lemma and celltype == "prs.pass.ind.1.sg":
+                wikibase_lemma = rowdata['form_normalized']
+                print(f"Row {rowcount}: Found for lemma cell {celltype}: {rowdata['form_normalized']}")
+
+
+            # Create form
+
+            form = Form()
+            form.representations.set(language='la', value=rowdata['form_normalized'])
+            form.grammatical_features = cellfeatures[celltype]
+            form.claims.add(datatypes.MonolingualText(prop_nr="11", language="la", text=rowdata['orth_form']))
+            form.claims.add(datatypes.String(prop_nr="12", value=rowdata['analysed_orth_form']))
+            form.claims.add(datatypes.String(prop_nr="13", value=celltype))
+            new_lexeme.forms.add(form)
+
+    if not wikibase_lemma:
+        print(f"flexeme {flexeme} has only #DEF# in 1.p. present, skipped in this run.")
+        with open("data/enhanced_forms_skipped_flexemes_flexemes_upload.txt", "a") as logfile:
+            logfile.write(f"{flexeme}\n")
+        continue
+    new_lexeme.lemmas.set(language="la", value=wikibase_lemma)
+    # with open("test_flexeme_entry.json", "w") as jsonfile:
+    #     json.dump(new_lexeme.get_json(), jsonfile, indent=2)
+    new_lexeme.write()
+    with open("mappings/prinparlat_wikibase_mapping_flexemes.csv", "a") as mappingfile:
+        mappingfile.write(f"{rowdata['flexeme']}\t{rowdata['lexeme']}\t{rowdata['lila_id_lemma']}\t{new_lexeme.id}\n")
+    print(f"Sucessfully created Lexeme https://lilamorph.wikibase.cloud/entity/{new_lexeme.id} and added {rowcount} forms.")
+    time.sleep(0.34)
+sys.exit()
---
+++ first_tests/upload_prinparlat_lexemes_v2d.py
@@ -0,0 +1,315 @@
+import csv, sys, time, re, json, requests
+import config_private # bot user and pwd from hidden file
+
+# import mwclient # mediawiki api client (will be used for itemdata > 1MB
+# lilamorph_api = mwclient.Site('lilamorph.wikibase.cloud')
+# login = lilamorph_api.login(username=config_private.wbuser, password=config_private.wbpwd)
+# csrfquery = lilamorph_api.api('query', meta='tokens')
+# lilamorph_api_token = csrfquery['query']['tokens']['csrftoken']
+# print("Got fresh CSRF token for lilamorph.wikibase.cloud.")
+
+# wikibaseintegrator for lilamorph wikibase
+from wikibaseintegrator import WikibaseIntegrator, wbi_login, datatypes
+from wikibaseintegrator.models import Reference, References, Form, Sense
+from wikibaseintegrator.wbi_enums import ActionIfExists
+from wikibaseintegrator.wbi_config import config
+config['MEDIAWIKI_API_URL'] = "https://lilamorph.wikibase.cloud/w/api.php"
+config['USER_AGENT'] = "upload_prinparlat_lexemes.py"
+print("Getting logged into lilamorph wikibase...")
+lilamorph = WikibaseIntegrator(login=wbi_login.Login(user=config_private.wbuser, password=config_private.wbpwd))
+print("Login successful.")
+
+
+
+def write_lexeme(new_lexeme):
+    done = False
+    attempts = 0
+    while not done and attempts < 3:
+        attempts += 1
+        try:
+            new_lexeme.write()
+            with open("mappings/prinparlat_wikibase_mapping_lexemes_v2c.csv", "a") as mappingfile:
+                mappingfile.write(f"{lexeme_id}\t{new_lexeme.id}\n")
+            print(
+                f"Sucessfully written to Lexeme https://lilamorph.wikibase.cloud/entity/{new_lexeme.id}.")
+            return new_lexeme.id
+        except Exception as ex:
+            print(str(ex))
+            errortext = str(ex).replace("\n", " ")
+            with open('data/failed_lexemes_v2c.txt', 'a') as errorlog:
+                errorlog.write(f'{lexeme_id}\t{rowcount}\t{errortext}\n')
+        return False
+
+# get prinparlat cell descriptors mapping
+with open("mappings/paralex_featurevalue_mapping.csv") as file:
+    mappingrows = csv.reader(file, delimiter="\t")
+    leipzig_qid = {}
+    for row in mappingrows:
+        leipzig_qid[row[0]] = row[1]
+print(f"Leipzig cell descriptors mapping: {leipzig_qid}")
+
+# get prinparlat cell types default order
+with open("mappings/celltypes_sorted.txt") as file:
+    celltypes_sorted = file.read().split("\n")
+cellfeatures = {}
+for celltype in celltypes_sorted:
+    cellfeatures[celltype] = []
+    for val in celltype.split("."):
+        cellfeatures[celltype].append(leipzig_qid[val])
+print(f"cellfeatures dict: {cellfeatures}")
+
+# get prinparlat cell types default order
+with open("data/prinparlat_lexemes_canonical_forms.csv") as file:
+    mappingrows = csv.DictReader(file, delimiter="\t")
+    canonicalforms = {}
+    for row in mappingrows:
+        canonicalforms[row['lexeme_id']] = row['label']
+print(f"Loaded canonical forms for {len(canonicalforms)} prinparlat lemma id.")
+
+# read mappingfile (lexemes processed before)
+with open("mappings/prinparlat_wikibase_mapping_lexemes_v2c.csv", "r") as mappingfile:
+    mappingrows = csv.DictReader(mappingfile, delimiter="\t")
+    done_lexeme_ids = []
+    for row in mappingrows:
+        done_lexeme_ids.append(row['lexeme'])
+    print(f"{len(done_lexeme_ids)} lexeme IDs have been processed in former runs: {done_lexeme_ids}")
+
+# build lexemes dictionary
+with open("data/enhanced_forms.csv") as file:
+    not_found = 0
+    csvrows = csv.DictReader(file, delimiter=",")
+    lexemes = {}
+    for row in csvrows:
+        lexeme_id = row['lexeme']
+        if "#DEF#" in row['form_normalized']:
+            continue
+        if row['flexeme'] == "": # flexeme id cannot be empty
+            row['flexeme'] = "0"
+            print(row)
+        if lexeme_id not in canonicalforms:
+            print(f"Lexeme {lexeme_id} not found in canonical forms list.")
+            sys.exit()
+        if lexeme_id not in lexemes:
+            lexemes[lexeme_id] = {'canonical': canonicalforms[lexeme_id], 'cells': {}}
+        if row['cell'] not in lexemes[lexeme_id]['cells']:
+            lexemes[lexeme_id]['cells'][row['cell']] = {'rows': [], 'conflations': {}}
+        lexemes[lexeme_id]['cells'][row['cell']]['rows'].append(row)
+    print(f"lexemes lexicon built: ({len(lexemes)} lexemes).")
+
+# build form conflations in lexemes
+for lexeme_id in lexemes:
+    for cell in lexemes[lexeme_id]['cells']:
+        cell_amount = len(lexemes[lexeme_id]['cells'][cell]['rows'])
+        if cell_amount == 1:
+            continue
+        conflations = {}
+        conflated = []
+        cellcount = 0
+        while cellcount < cell_amount:
+            target_row = lexemes[lexeme_id]['cells'][cell]['rows'][cellcount]
+            # print(f"target cell: {target_row}")
+            if f"{target_row['flexeme']}@{target_row['analysed_orth_form']}" not in conflated: # if not already part of a conflation
+                for source_row in lexemes[lexeme_id]['cells'][cell]['rows']:
+                    if source_row['flexeme'] != target_row['flexeme'] and source_row['orth_form'] == target_row['orth_form'] and source_row['analysed_orth_form'] == target_row['analysed_orth_form']:
+                        if f"{target_row['analysed_orth_form']}" not in conflations:
+                            conflations[f"{target_row['analysed_orth_form']}"] = [target_row['flexeme']]
+                        conflations[f"{target_row['analysed_orth_form']}"].append(source_row['flexeme'])
+                        conflated.append(f"{source_row['flexeme']}@{source_row['analysed_orth_form']}") # unique combination of flex_id, orth_form and analyzed_orth_form
+            cellcount += 1
+        lexemes[lexeme_id]['cells'][cell]['conflations'] = conflations
+print("Conflations dictionary built.")
+
+with open('data/v2_conflation.json', 'w') as jsonfile:
+    json.dump(lexemes["a0200"], jsonfile, indent=2)
+
+
+
+lexeme_count = 0
+
+# for lexeme_id in lexemes:
+for lexeme_id in ["a3338","c4706","p0736"]:
+    lexeme_count += 1
+
+    print(f"\nNow processing lexeme {lexeme_count}/{len(lexemes)}: {lexeme_id}, '{lexemes[lexeme_id]['canonical']}'")
+    if lexeme_id in done_lexeme_ids:
+        print(f"{lexeme_id} lexeme_id has been done before (found in mappingfile), skipped.")
+        continue
+
+    rowcount = 0
+    formscount = 0
+    new_forms = []
+    queryurl = f"https://lilamorph.wikibase.cloud/query/sparql?format=json&query=PREFIX%20lmwb%3A%20%3Chttps%3A%2F%2Flilamorph.wikibase.cloud%2Fentity%2F%3E%0APREFIX%20lmdp%3A%20%3Chttps%3A%2F%2Flilamorph.wikibase.cloud%2Fprop%2Fdirect%2F%3E%0A%0A%20select%20%0A%20%20%3Flexeme%20%3Fprinparlat_lexeme%20%3Fcell%20%3Fform%20%3Fform_rep%20%3Fanalyzed_orth_form%20(group_concat(%3Fflexeme)%20as%20%3Fflexemes)%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%0A%20%20%20%20%20where%20%7B%20%20%20%20values%20%3Fprinparlat_lexeme%20%7B%22{lexeme_id}%22%7D%0A%20%20%3Flexeme%20lmdp%3AP14%20lmwb%3AQ14%3B%20lmdp%3AP8%20%3Fprinparlat_lexeme.%0A%20%20%3Flexeme%20ontolex%3AlexicalForm%20%3Fform.%0A%20%20%3Fform%20lmdp%3AP6%20%3Fflexeme%3B%20ontolex%3Arepresentation%20%3Fform_rep%3B%20lmdp%3AP13%20%3Fcell%3B%20lmdp%3AP12%20%3Fanalyzed_orth_form.%0A%20%20%7D%20group%20by%20%3Flexeme%20%3Fprinparlat_lexeme%20%3Fcell%20%3Fform%20%3Fform_rep%20%3Fanalyzed_orth_form%20%3Fflexemes%20"
+    wblexemedata = requests.get(queryurl).json()['results']['bindings']
+    print(f"Got {len(wblexemedata)} rows of formdata for {lexeme_id}")
+    wblexeme_id = wblexemedata[0]['lexeme']['value'].replace("https://lilamorph.wikibase.cloud/entity/","")
+    lexeme_entity = None
+    # ask for forms data from the formtypes (celltypes) canonical order list
+    for celltype in celltypes_sorted:
+        for cell in lexemes[lexeme_id]['cells']:
+            if cell != celltype: # no celltype match
+                continue
+            done_forms = []
+            for row in lexemes[lexeme_id]['cells'][cell]['rows']:
+
+                rowcount += 1
+                if row['analysed_orth_form'] in done_forms:
+                    continue
+                if row['analysed_orth_form'] in lexemes[lexeme_id]['cells'][cell]['conflations']:
+                    cell_flex_ids = lexemes[lexeme_id]['cells'][cell]['conflations'][row['analysed_orth_form']]
+                else:
+                    cell_flex_ids = [row['flexeme']]
+                done_forms.append(row['analysed_orth_form'])
+
+
+                # Check form
+                row_is_there = False
+                for wbform in wblexemedata:
+                    if wbform['cell']['value'] == celltype:
+                        if wbform['form_rep']['value'] == row['form_normalized'] and wbform['analyzed_orth_form']['value'] == row['analysed_orth_form']:
+                            if row['flexeme'] not in wbform['flexemes']['value'].split(" "):
+                                print(f"found BIG PROBLEM in row (flexeme id not listed in formrep group) {row}")
+                                sys.exit()
+                            else:
+                                # print(f"found row {row} in Wikibase.")
+                                row_is_there = True
+                                break
+                if row_is_there:
+                    continue
+
+
+                # build new form (wikibaseintegrator version)
+                form = Form()
+                form.representations.set(language='la', value=row['form_normalized'])
+                form.grammatical_features = cellfeatures[celltype]
+                for flex_id in cell_flex_ids:
+                    form.claims.add(datatypes.ExternalID(prop_nr="P6", value=flex_id), action_if_exists=ActionIfExists.APPEND_OR_REPLACE)
+                form.claims.add(datatypes.MonolingualText(prop_nr="P11", language="la", text=row['orth_form']))
+                form.claims.add(datatypes.String(prop_nr="P12", value=row['analysed_orth_form']))
+                form.claims.add(datatypes.String(prop_nr="P13", value=celltype))
+                new_forms.append(form)
+
+                # # form creation (version for mediawiki api, used for lexemes bigger than 2MB
+                # formdata = {
+                #     "representations": {"la": {"language": "la", "value": row['form_normalized']}},
+                #     "grammaticalFeatures": cellfeatures[celltype]
+                # }
+                # formcreation = lilamorph_api.post('wbladdform', token=lilamorph_api_token, format="json",
+                #                                   lexemeId=wblexeme_id, bot=1, data=json.dumps(formdata))
+                # formid = formcreation['form']['id']
+                # print(f'\n{rowcount}: Form creation for ' + wblexeme_id + ': success: ' + formid)
+                # time.sleep(0.6)
+                # claims = []
+                # for flex_id in cell_flex_ids:
+                #     claims.append(("P6", f'"{flex_id}"'))
+                # claims += [
+                #     ("P11", json.dumps({"language": "la", "text": row['orth_form']})),
+                #     ("P12", f'"{row["analysed_orth_form"]}"'),
+                #     ("P13", f'"{celltype}"')
+                # ]
+                # for claim in claims:
+                #     claimcreation = lilamorph_api.post('wbcreateclaim', token=lilamorph_api_token, entity=formid,
+                #                                        property=claim[0], snaktype="value", bot=1, value=claim[1])
+                #     if claimcreation['success'] == 1:
+                #         print(f'Claim {claim[0]} - {claim[1]}: success.')
+                #         time.sleep(.3)
+                #     else:
+                #         print(f"Failed to write claim {claim[0]} - {claim[1]}")
+                #         sys.exit()
+
+
+    # with open("test_lexeme_entry.json", "w") as jsonfile:
+    #     json.dump(new_lexeme.get_json(), jsonfile, indent=2)
+    if len(new_forms) > 0:
+        if not lexeme_entity:
+            lexeme_entity = lilamorph.lexeme.new(language="Q3", lexical_category="Q5")  # latin verb
+            lexeme_entity.lemmas.set(language="la", value=lexemes[lexeme_id]['canonical'])  # wikibase lemma
+            lexeme_entity.claims.add(datatypes.Item(prop_nr="P14", value="Q14"))  # collection
+            lexeme_entity.claims.add(datatypes.ExternalID(prop_nr="P8", value=lexeme_id))  # prinparlat lexeme id
+            lila_lemma_id = lexemes[lexeme_id]['cells'][cell]['rows'][0]['lila_id_lemma']
+            lexeme_entity.claims.add(
+                datatypes.ExternalID(prop_nr="P1", value=f"lemma/{lila_lemma_id}"))  # lila lemma id
+
+
+        if len(new_forms) > 500:
+
+            for f in new_forms[:500]:
+                lexeme_entity.forms.add(f)
+            print(f"Will write {len(new_forms[:500])} forms to lexeme.")
+            lexeme_entity_id = write_lexeme(lexeme_entity)
+            if lexeme_entity_id:
+                print(f"First write operation to {lexeme_entity_id} successful, with {len(wblexemedata)+len(new_forms[:250])} forms.")
+                del new_forms [:500]
+                json_lexeme = \
+                    requests.get(f"https://lilamorph.wikibase.cloud/wiki/Special:EntityData/{lexeme_entity_id}.json").json()[
+                        'entities'][
+                        wblexeme_id]
+                json_lexeme['forms'] = []
+                lexeme_entity = lilamorph.lexeme.from_json(json_lexeme)
+                time.sleep(2)
+            else:
+                print(f"*** Failed writing.")
+                continue
+
+        for f in new_forms:
+            lexeme_entity.forms.add(f)
+        print(f"Will write {len(new_forms)} forms to lexeme.")
+        lexeme_entity_id = write_lexeme(lexeme_entity)
+        if lexeme_entity_id:
+            print(
+                f"Write operation to {lexeme_entity_id} successful, with {len(wblexemedata) + len(new_forms)} forms.")
+        else:
+            print(f"*** Failed writing.")
+    else:
+        with open("mappings/prinparlat_wikibase_mapping_lexemes_v2c.csv", "a") as mappingfile:
+            mappingfile.write(f"{lexeme_id}\t{wblexeme_id}\n")
+        print("No change necessary for this lexeme.")
+    time.sleep(1)
+
+
+
+print("\nFinished.")
+sys.exit()
+
+# nice code:
+
+            #     csrfquery = lilamorph_api.api('query', meta='tokens')
+            #     lilamorph_api_token = csrfquery['query']['tokens']['csrftoken']
+            #     print("Got fresh CSRF token for lilamorph.wikibase.cloud.")
+            #     json_lexeme = new_lexeme.get_json()
+            #     rowcount = 0
+            #     for form in json_lexeme['forms']:
+            #         rowcount += 1
+            #         formdata = {
+            #             "representations": form['representations'],
+            #             "grammaticalFeatures": form['grammaticalFeatures']
+            #         }
+            #         formcreation = lilamorph_api.post('wbladdform', token=lilamorph_api_token, format="json",
+            #                                       lexemeId=wblexeme_id, bot=1, data=json.dumps(formdata))
+            #         formid = formcreation['form']['id']
+            #         print(f'[{rowcount}/{len(json_lexeme['forms'])}]: Form creation for ' + wblexeme_id + ': success: ' + formid)
+            #         time.sleep(0.41)
+            #         for prop in form['claims']:
+            #             for claim in form['claims'][prop]:
+            #                 if claim['mainsnak']['datatype'] == "wikibase-item":
+            #                     target_nid = claim['mainsnak']['datavalue']['value']['numeric-id']
+            #                     claimvalue = json.dumps({"entity-type": "item", "numeric-id": target_nid})
+            #                 elif claim['mainsnak']['datatype'] == "monolingualtext":
+            #                     claimvalue = json.dumps(claim['mainsnak']['datavalue']['value'])
+            #                 elif claim['mainsnak']['datatype'] == "string":
+            #                     claimvalue = f'"{claim['mainsnak']['datavalue']['value']}"'
+            #                 else:
+            #                     print(f"datatype {claim['mainsnak']['datatype']} not implemented...")
+            #                     sys.exit()
+            #                 claimcreation = lilamorph_api.post('wbcreateclaim', token=lilamorph_api_token, entity=formid,
+            #                                                property=prop, snaktype="value", bot=1, value=claimvalue)
+            #                 if claimcreation['success'] == 1:
+            #                     print(f'Claim {prop} - {claimvalue}: success.')
+            #                     time.sleep(.3)
+            #                 else:
+            #                     print(f"Failed to write claim {prop} - {claimvalue}")
+            #                     sys.exit()
+            #     return lexeme_entity_id
+            #
+            # print(errortext)
+            # print("Failed to write data to Wikibase.")
+            # sys.exit()
