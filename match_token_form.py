ef93a4d43e69d7d8acb11ef79b76ce00ad8c9001
David Lindemann <34068491+dlindem@users.noreply.github.com>
2025-10-29 20:58:43 +0100

    Final version for production of version 3

--- match_token_form.py
+++ match_token_form.py
@@ -1,9 +1,10 @@
-import csv, requests
+import csv
 import time
 import config_private
 from wikibaseintegrator import WikibaseIntegrator, wbi_login
 from wikibaseintegrator.wbi_config import config
 from wikibaseintegrator.wbi_helpers import execute_sparql_query
+
 config['MEDIAWIKI_API_URL'] = "https://lilamorph.wikibase.cloud/w/api.php"
 config['USER_AGENT'] = "upload_prinparlat_lexemes.py"
 config['SPARQL_ENDPOINT_URL'] = "https://lilamorph.wikibase.cloud/query/sparql"
@@ -11,23 +12,6 @@ print("Getting logged into lilamorph wikibase...")
 lilamorph = WikibaseIntegrator(login=wbi_login.Login(user=config_private.wbuser, password=config_private.wbpwd))
 print("Login successful.")
 
-# data = pd.read_csv("data/lemmata_wikibase.csv", sep="\t")
-# length = data.shape[0]
-# rowcount = 1
-# thislemma = None
-# lemmadict = {}
-# while rowcount < length:
-#     rowdata = data.iloc[rowcount].to_dict()
-#     # print(rowdata)
-#     lila_lemma = str(rowdata['lila_lemma_id']).replace("lemma/","")
-#     if lila_lemma != thislemma: # new lemma list begins
-#         thislemma = lila_lemma
-#         lemmadict[thislemma] = [rowdata]
-#     else:
-#         lemmadict[thislemma].append(rowdata)
-#     rowcount += 1
-
-# print(list(lemmadict.keys()))
 
 def get_wikibase_forms(lila_lemma_id):
     query = """PREFIX lmwb: <https://lilamorph.wikibase.cloud/entity/>
@@ -51,10 +35,10 @@ def get_wikibase_forms(lila_lemma_id):
     # print(query)
 
     bindings = execute_sparql_query(query=query)['results']['bindings']
-
     print(f"Got {len(bindings)} results from Wikibase SPARQL endpoint.")
     time.sleep(.3)
     return bindings
+
 
 with open('mappings/ITTB_tokens_wikibase.csv') as logfile:
     token_items = {}
@@ -71,7 +55,7 @@ with open("data/ITTB_verb_tokendata_for_upload.csv") as file:
         tokenrowcount += 1
 
         match_for_token = False
-        lila_lemmas = row['lila_lemmas'].split("|") # matching lila lemma(s), according to token annotation
+        lila_lemmas = row['lila_lemmas'].split("|")  # matching lila lemma(s), according to token annotation
 
         print(f"\n[{tokenrowcount}] Now processing lemma IDs: {lila_lemmas}...")
         for lila_lemma in lila_lemmas:
@@ -80,81 +64,48 @@ with open("data/ITTB_verb_tokendata_for_upload.csv") as file:
                 print(f"Retrieving form data for LiLa Lemma '{lila_lemma}' from Wikibase...")
                 lemmadict[lila_lemma] = get_wikibase_forms(lila_lemma)
 
-        if len(lemmadict[lila_lemma]) == 0:
-            print("Did not find this lemma on Wikibase.")
-            with open('data/lemma_not_found_lexemedict_v3.csv', 'a') as outfile:
-                outfile.write(f"{lila_lemma}\n")
-            continue
+            if len(lemmadict[lila_lemma]) == 0:
+                print("Did not find this lemma on Wikibase.")
+                with open('data/lemma_not_found_lexemedict_v3.csv', 'a') as outfile:
+                    outfile.write(f"{lila_lemma}\n")
+                continue
 
-        token_features = sorted(row['features'].split("|"))
+            token_features = sorted(row['features'].split("|"))
             # for ignore_feature in ["VerbForm#Fin", "Degree#Pos", "Aspect#Imp"]:
             #     if ignore_feature in token_features:
             #         token_features.remove(ignore_feature)
 
-        # matching algorithm
-        for formdata in lemmadict[lila_lemma]:
-            form_features = sorted(formdata['udp_list']['value'].split("|"))
+            # matching algorithm
+            for formdata in lemmadict[lila_lemma]:
+                form_features = sorted(formdata['udp_list']['value'].split("|"))
 
-            for form_flex_id in formdata['form_flexeme_ids']['value'].split("|"):
-                if form_flex_id not in formdata['lila_id_linked_flexeme_ids']['value'].split("|"):
-                    continue # do not look at forms that do not have a flexeme id associated to the lila lemma
-                             # example: https://lilamorph.wikibase.cloud/wiki/Lexeme:L33585
-            if formdata['form_rep']['value'] == row['tokenLabel'].replace("v", "u"):
-                # match according to form representation
-                for ignore_feature in ["Aspect#Imp"]: # those that are not coherently there (on the tokens)
-                    if ignore_feature in form_features:
-                        form_features.remove(ignore_feature)
-                # check if morph annotation is also matching
-                match = True
-                for feature in form_features:
-                    if feature not in token_features:
-                        match = False
-                        break
-                if match:
-                    match_for_token = True
-                    matchcount += 1
-                    print(
-                        f"\nMatching token: {row['tokenLabel']} {token_features}\nMatching form:  {formdata['form_rep']['value']} {form_features}")
-                    with open('data/matching_tokens_lexemedict_v3.csv', 'a') as outfile:
-                        outfile.write(
-                            f"{row['token']}\t{row['tokenLabel']}\t{token_items[row['token']]}\t{formdata['form']['value']}\t{lila_lemma}\t{'|'.join(token_features)}\n")
+                for form_flex_id in formdata['form_flexeme_ids']['value'].split("|"):
+                    if form_flex_id not in formdata['lila_id_linked_flexeme_ids']['value'].split("|"):
+                        continue  # do not look at forms that do not have a flexeme id associated to the lila lemma
+                        # example: https://lilamorph.wikibase.cloud/wiki/Lexeme:L33585
+                if formdata['form_rep']['value'] == row['tokenLabel'].replace("v", "u"):
+                    # match according to form representation
+                    for ignore_feature in ["Aspect#Imp"]:  # those that are not coherently there (on the tokens)
+                        if ignore_feature in form_features:
+                            form_features.remove(ignore_feature)
+                    # check if morph annotation is also matching
+                    match = True
+                    for feature in form_features:
+                        if feature not in token_features:
+                            match = False
+                            break
+                    if match:
+                        match_for_token = True
+                        matchcount += 1
+                        print(
+                            f"\nMatching token: {row['tokenLabel']} {token_features}\nMatching form:  {formdata['form_rep']['value']} {form_features}")
+                        with open('data/matching_tokens_lexemedict_v3.csv', 'a') as outfile:
+                            outfile.write(
+                                f"{row['token']}\t{row['tokenLabel']}\t{token_items[row['token']]}\t{formdata['form']['value']}\t{lila_lemma}\t{'|'.join(token_features)}\n")
 
         if not match_for_token:
             with open('data/not_matching_tokens_lexemedict_v3.csv', 'a') as outfile:
-                outfile.write(f"{row['token']}\t{row['tokenLabel']}\tNO MATCH\t{lila_lemma}\t{'|'.join(token_features)}\n")
-
+                outfile.write(
+                    f"{row['token']}\t{row['tokenLabel']}\tNO MATCH\t{lila_lemma}\t{'|'.join(token_features)}\n")
 
 print(f"\nFound {matchcount} matches.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# query = """PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-#         PREFIX lmwb: <https://lilamorph.wikibase.cloud/entity/>
-#         PREFIX lmdp: <https://lilamorph.wikibase.cloud/prop/direct/>
-#
-#         select distinct ?lila_lemma_id ?flexeme ?form ?formrep
-#         (group_concat(distinct ?udp;SEPARATOR="|") as ?udp_list)
-#         where {
-#           ?flexeme lmdp:P1 ?lila_lemma_id.
-#           ?flexeme ontolex:lexicalForm ?form.
-#           ?form ontolex:representation ?formrep; wikibase:grammaticalFeature ?form_feat.
-#           ?form_feat lmdp:P9 [lmdp:P7 ?udp] .
-#         } group by ?lila_lemma_id ?flexeme ?form ?formrep ?udp_list"""
